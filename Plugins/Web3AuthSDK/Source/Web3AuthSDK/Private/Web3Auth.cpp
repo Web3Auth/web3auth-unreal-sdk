d7868422f36b9c235f4953dadc578eae60a2681f
Muhammad Usman <usman@wondertree.co>
2022-08-29 19:57:56 +0500

    iOS changes

--- Plugins/Web3AuthSDK/Source/Web3AuthSDK/Private/Web3Auth.cpp
+++ Plugins/Web3AuthSDK/Source/Web3AuthSDK/Private/Web3Auth.cpp
@@ -211,10 +211,6 @@ void AWeb3Auth::setResultUrl(FString hash) {
 			AWeb3Auth::loginEvent.ExecuteIfBound(web3AuthResponse);
 		});
 	}
-
-#if PLATFORM_IOS
-		[dismiss];
-#endif
 }
 
 template <typename StructType>
