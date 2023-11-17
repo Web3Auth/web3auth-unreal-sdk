f14ec2d57dc6fac26dbd4efb275f381421ab869b
metalurgical <97008724+metalurgical@users.noreply.github.com>
2023-11-17 11:08:24 +0200

    Update KeyStoreUtils.cpp

--- Plugins/Web3AuthSDK/Source/Web3AuthSDK/Private/KeyStoreUtils.cpp
+++ Plugins/Web3AuthSDK/Source/Web3AuthSDK/Private/KeyStoreUtils.cpp
@@ -1,5 +1,12 @@
 #include "KeyStoreUtils.h"
 
+// Note: Slot name and user index (2nd and 3rd parameters) should be configurable,
+// since a game could have multiple different profiles and users on the same machine.
+
+// Note: Class should have a lock on accessing the KeyValuePair collection. Probably is not thread-safe.
+
+// Note: This class should also most likely be a singleton that persists for the lifetime of the application
+
 UKeyStoreUtils::UKeyStoreUtils() {
 	StorageInstance = Cast<UWeb3StorageAdapter>(UGameplayStatics::LoadGameFromSlot(TEXT("Web3AuthDataSlot"), 0));
 	if(StorageInstance == nullptr) {
@@ -11,11 +18,21 @@ UKeyStoreUtils::~UKeyStoreUtils() {
 }
 
 void UKeyStoreUtils::Add(FString key, FString value) {
+	if (StorageInstance == nullptr)
+	{
+		// log error here
+		return;
+	}
 	StorageInstance->KeyValuePairs.Add(key, value);
 	UGameplayStatics::SaveGameToSlot(StorageInstance, TEXT("Web3AuthDataSlot"), 0);
 }
 
 FString UKeyStoreUtils::Get(FString key) {
+	if (StorageInstance == nullptr)
+	{
+		// log error here
+		return FString();
+	}
 	FString* value = StorageInstance->KeyValuePairs.Find(key);
 	if (value == nullptr) {
 		return FString();
@@ -25,6 +42,11 @@ FString UKeyStoreUtils::Get(FString key) {
 }
 
 void UKeyStoreUtils::Remove(FString key) {
+	if (StorageInstance == nullptr)
+	{
+		// log error here
+		return;
+	}
 	StorageInstance->KeyValuePairs.Remove(key);
 	UGameplayStatics::SaveGameToSlot(StorageInstance, TEXT("Web3AuthDataSlot"), 0);
 }
